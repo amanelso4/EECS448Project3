c737df7fdac9c050237c146d770ecbcee3fea5b1
Kevin Carlson <23kcarlson@ku.edu>
2020-10-20 01:25:48 -0500

    Works with master

--- game.js
+++ game.js
@@ -1,23 +1,68 @@
 var myCharacter;
 var ground;
+var obstacles;
+var tps =100;
 var key;
-var tps = 50; //This number is how many times it will run in one second.
-function startGame() {
-  myCharacter = new character();
-  ground = new component(900, 300, "green", 0, 400);
-  myGameArea.start();
+var myGameArea = {
+  context: null,
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 900;
+    this.canvas.height = 700; //the size of the game screen
+    this.context = this.canvas.getContext("2d");
+
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 1000/tps); //to make the game go faster or slower change this interval
+    window.addEventListener('keydown', function (e) {
+      key = e.key;
+     })
+     window.addEventListener('keyup', function (e) {
+       key = false;
+     })  
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 }
-function character(){
-  this.charGrounded = true;
-  this.yVelocity = 0;
-  this.gravity = 5;
-  this.jumpStrength = 250;
-  this.draw = new component(30, 50, "black", 10, 350);
-  this.move = function (){
-    if(this.draw.x <=20){
-      this.draw.x +=1;
+
+class Component {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+  update = function () {
+    let ctx = myGameArea.context;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  clear = function () {
+    let ctx = myGameArea.context;
+    ctx.clearRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function startGame() {
+  myGameArea.start();
+  myCharacter = new Character();
+  ground = new Component(900, 300, "green", 0, 400);
+  obstacles = [new Obstacle()];
+}
+class Character extends Component{
+  charGrounded = true;
+  yVelocity = 0;
+  gravity = 5;
+  jumpStrength = 400;
+  constructor(){
+    super(30, 50, "black", 10, 350);
+  }
+  move = function (){
+    if(this.x <=50){
+      this.x +=1;
     }
-    this.charGrounded = this.draw.y+this.draw.height>=ground.y;
+    this.charGrounded = this.y+this.height>=ground.y;
     if(this.charGrounded){
       this.yVelocity=0;
       if(key == "w"){
@@ -27,51 +72,32 @@ function character(){
     else{
       this.yVelocity += this.gravity;
     }
-    //this.draw.y += this.yVelocity/tps; //smoother, but leaves artifacts. Would have to clear entire game area to fix
-    this.draw.y += Math.round(this.yVelocity/tps); //still occasionally leaves artifacts.
+    //this.y += this.yVelocity/tps; //smoother, but leaves artifacts. Would have to clear entire game area to fix
+    this.y += Math.round(this.yVelocity/tps); //still occasionally leaves artifacts.
+    this.update();
   }
 }
-function component(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.x = x;
-  this.y = y;
-  this.update = function() {
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+class Obstacle extends Component {
+
+  constructor() {
+    super(30, 50, "red", myGameArea.canvas.width, 350);
   }
-  this.clear = function() {
-    ctx = myGameArea.context;
-    ctx.clearRect(this.x, this.y, this.width, this.height);
-  }
+
+  move = function () {
+    this.x -= 1;
+  };
 }
 
-var myGameArea = {
-  canvas : document.createElement("canvas"),
-  start : function() {
-    this.canvas.width = 900;
-    this.canvas.height = 700; //the size of the game screen
-    this.context = this.canvas.getContext("2d");
-
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 1000/tps);
-    window.addEventListener('keydown', function (e) {
-     key = e.key;
-    })
-    window.addEventListener('keyup', function (e) {
-      key = false;
-    })  
-  },
-  clear : function() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+function updateGameArea() {
+  myCharacter.clear(); //we use myCharacter.clear() instead of myGameArea.clear() because we don't want the ground to clear
+  for (let ob of obstacles) {
+    ob.clear();
   }
+  ground.x += 1;
+  ground.update();
+  myCharacter.move();
+  for (let ob of obstacles) {
+    ob.move();
+    ob.update();
   }
-  
-  function updateGameArea() {
-    myCharacter.draw.clear(); //we use myCharacter.draw.clear() instead of myGameArea.clear() because we don't want the ground to clear
-    myCharacter.move();
-    ground.x += 1;
-    myCharacter.draw.update();
-    ground.update();
-  }
+}
