144d6b4714bc98a545f3ef8042544b97f0867bd9
Kevin Carlson <23kcarlson@ku.edu>
2020-10-20 01:08:03 -0500

    Added player movement

--- game.js
+++ game.js
@@ -1,12 +1,36 @@
 var myCharacter;
 var ground;
-
+var key;
+var tps = 50; //This number is how many times it will run in one second.
 function startGame() {
-  myCharacter = new component(30, 50, "black", 10, 350);
+  myCharacter = new character();
   ground = new component(900, 300, "green", 0, 400);
   myGameArea.start();
 }
-
+function character(){
+  this.charGrounded = true;
+  this.yVelocity = 0;
+  this.gravity = 5;
+  this.jumpStrength = 250;
+  this.draw = new component(30, 50, "black", 10, 350);
+  this.move = function (){
+    if(this.draw.x <=20){
+      this.draw.x +=1;
+    }
+    this.charGrounded = this.draw.y+this.draw.height>=ground.y;
+    if(this.charGrounded){
+      this.yVelocity=0;
+      if(key == "w"){
+        this.yVelocity = 0-this.jumpStrength;
+      }
+    }
+    else{
+      this.yVelocity += this.gravity;
+    }
+    //this.draw.y += this.yVelocity/tps; //smoother, but leaves artifacts. Would have to clear entire game area to fix
+    this.draw.y += Math.round(this.yVelocity/tps); //still occasionally leaves artifacts.
+  }
+}
 function component(width, height, color, x, y) {
   this.width = width;
   this.height = height;
@@ -31,17 +55,23 @@ var myGameArea = {
     this.context = this.canvas.getContext("2d");
 
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20); //to make the game go faster or slower change this interval
+    this.interval = setInterval(updateGameArea, 1000/tps);
+    window.addEventListener('keydown', function (e) {
+     key = e.key;
+    })
+    window.addEventListener('keyup', function (e) {
+      key = false;
+    })  
   },
   clear : function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
   }
-
+  
   function updateGameArea() {
-    myCharacter.clear(); //we use myCharacter.clear() instead of myGameArea.clear() because we don't want the ground to clear
-    myCharacter.x += 1;
+    myCharacter.draw.clear(); //we use myCharacter.draw.clear() instead of myGameArea.clear() because we don't want the ground to clear
+    myCharacter.move();
     ground.x += 1;
-    myCharacter.update();
+    myCharacter.draw.update();
     ground.update();
   }
